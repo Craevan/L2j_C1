7162434588b0f561d6f18b1a8ec6ef22925af75a
Crevan <a.siverskii@gmail.com>
2022-07-31 11:13:49 +0300

    update LoginController

--- src/java/ru/crevan/loginserver/LoginController.java
+++ src/java/ru/crevan/loginserver/LoginController.java
@@ -8,13 +8,13 @@ import java.util.Map;
 public class LoginController {
 
     private final Map<String, Integer> logins = new HashMap<>();
-    private final Map<String, Connection> accountinGameServer = new HashMap<>();
-    private final Map<String, Socket> accountsinloginServer;
+    private final Map<String, Connection> accountsInGameServer = new HashMap<>();
+    private final Map<String, Socket> accountsInLoginServer;
     private final Map<String, Integer> accessLvl;
     private int maxAllowedPlayers;
 
     private LoginController() {
-        this.accountsinloginServer = new HashMap<>();
+        this.accountsInLoginServer = new HashMap<>();
         this.accessLvl = new HashMap<>();
     }
 
@@ -24,5 +24,71 @@ public class LoginController {
 
     private static class LoginControllerHolder {
         private static final LoginController HOLDER_INSTANCE = new LoginController();
+    }
+
+    public int assignSessionKeyToLogin(final String account, final int accessLvl, final Socket socket) {
+        int key = (int) System.currentTimeMillis() & 0xFFFFFF;
+        logins.put(account, key);
+        accountsInLoginServer.put(account, socket);
+        this.accessLvl.put(account, accessLvl);
+        return key;
+    }
+
+    public void addGameServerLogin(final String account, final Connection connection) {
+        accountsInGameServer.put(account, connection);
+    }
+
+    public void removeGameServerLogin(final String account) {
+        if (account != null) {
+            logins.remove(account);
+            accountsInGameServer.remove(account);
+        }
+    }
+
+    public void removeLoginServerLogin(final String account) {
+        if (account != null) {
+            accountsInLoginServer.remove(account);
+        }
+    }
+
+    public boolean isAccountInLoginServer(final String account) {
+        return accountsInLoginServer.containsKey(account);
+    }
+
+    public boolean isAccountInGameServer(final String account) {
+        return accountsInGameServer.containsKey(account);
+    }
+
+    public int getKeyForAccount(final String account) {
+        final Integer result = logins.get(account);
+        return result != null ? result : 0;
+    }
+
+    public int getOnlinePlayerCount() {
+        return accountsInGameServer.size();
+    }
+
+    public int getMaxAllowedPlayers() {
+        return maxAllowedPlayers;
+    }
+
+    public void setMaxAllowedPlayers(final int maxAllowedPlayers) {
+        this.maxAllowedPlayers = maxAllowedPlayers;
+    }
+
+    public boolean isLoginPossible(final int accessLvl) {
+        return (accountsInGameServer.size() < maxAllowedPlayers) || (accessLvl >= 50);
+    }
+
+    public int getGmAccessLevel(final String login) {
+        return accessLvl.get(login);
+    }
+
+    public Connection getClientConnection(final String login) {
+        return accountsInGameServer.get(login);
+    }
+
+    public Socket getLoginServerConnection(final String login) {
+        return accountsInLoginServer.get(login);
     }
 }
