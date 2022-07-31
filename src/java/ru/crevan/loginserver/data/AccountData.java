3ba2042cb894d21e5090387e34fd411b2665de05
Crevan <a.siverskii@gmail.com>
2022-07-31 21:25:49 +0300

    [update]: add getAccessLevel() method to AccountData class; add stub for loginValid() method

--- src/java/ru/crevan/loginserver/data/AccountData.java
+++ src/java/ru/crevan/loginserver/data/AccountData.java
@@ -1,7 +1,23 @@
 package java.ru.crevan.loginserver.data;
 
+import java.net.InetAddress;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class AccountData {
+
+    private static final Map<String, Integer> accessLevels = new ConcurrentHashMap<>();
     //stub
-    public AccountData(boolean autoCreateAccounts) {
+    public AccountData(final boolean autoCreateAccounts) {
+    }
+
+    public int getAccessLevel(final String account) {
+        return accessLevels.get(account);
+    }
+
+
+    public boolean loginValid(final String account, final String password, final InetAddress inetAddress) {
+        // TODO
+        return false;
     }
 }
